f852ca50af46e5b0802cd46b1920960854861285
Sotiri Komissopoulos <skomissopoulos@gmail.com>
2017-03-12 00:05:07 -0600

    added ra_client.h

---
+++ RemoteAttestation/isv_app/ra_client.h
@@ -0,0 +1,34 @@
+#ifndef RA_CLIENT_H
+#define RA_CLIENT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct {
+    mbedtls_ssl_config conf;
+    mbedtls_ctr_drbg_context ctr_drbg;
+    mbedtls_entropy_context entropy;
+
+    mbedtls_ssl_session saved_session;
+    mbedtls_net_context server_fd;
+    mbedtls_ssl_context ssl;
+
+    mbedtls_x509_crt cacert;
+    mbedtls_x509_crt clicert;
+    mbedtls_pk_context pkey;
+} ssl_state_t;
+
+ssl_state_t *sotiri_connect(client_opt_t *_opt);
+int sotiri_send(client_opt_t *opt, ssl_state_t *ssl_state, char *headers[], int n_headers, const char *body);
+int sotiri_recv(client_opt_t *opt, ssl_state_t *ssl_state, char *output, size_t output_len);
+void sotiri_close_notify(client_opt_t *opt, ssl_state_t *ssl_state);
+void sotiri_exit(int err_code, ssl_state_t *ssl_state);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
